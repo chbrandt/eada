b6e4a0144d25a5118fb28aff24f251cde104725e
Carlos Brandt <carloshenriquebrandt@gmail.com>
2016-02-22 20:25:12 +0100

    Added cli for ssa service.

--- eada/vo/specsearch.py
+++ eada/vo/specsearch.py
@@ -52,8 +52,64 @@ class Aux:
         logging.debug("Selected columns: %s" % cols)
         return cols
 
+    @staticmethod
+    def open_file(file,format):
+        """
+        Read table from FITS/VOTable file
+        """
+        from astropy.table import Table
+        tab = None
+        if format is 'fits':
+            # I am assuming the table of interest it at the first BinTableHDU
+            hdu_index = 1
+            tab = Table.read(file,format=format,hdu=hdu_index)
+        elif format is 'votable':
+            tab = Table.read(file,format=format)
+        else:
+            print("Format {} not supported (using Astropy's Table).".format(format))
+        return tab
+
+    def options(key):
+        _options = {
+            'format' : """
+                all       all formats available
+                compliant any SSA data model compliant format
+                native    the native project specific format for the spectrum
+                graphic   any of the graphics formats: JPEG, PNG, GIF
+                votable   the SSA VOTable format
+                fits      the SSA-compliant FITS format
+                xml       the SSA native XML serialization
+                metadata  no images requested; only an empty table with fields
+                          properly specified
+                """,
+        }
+        if not _options.has_key(key):
+            print("Don't know such option. Can be a typo or I'm missing it. Check the docs.")
+        print _options[key]
+
+    def resolve_name(name):
+        """
+        Resolve object name, return its ICRS position in degrees
+        """
+        from astropy.coordinates import get_icrs_coordinates as get_coords
+        try:
+            icrs = get_coords(name)
+            pos = (icrs.ra.value,icrs.dec.value)
+        except:
+            pos = None
+        return pos
+
+    def download_spec(record,dir='/tmp/ssafiles'):
+        from os import mkdir
+        from os import path
+        f = record.format
+        mkdir(dir)
+        record.cachedataset(dir=dir)
+        return path.join(dir,record.make_dataset_filename())
+
+
 # --
-def specsearch(ra,dec,radius,url,timeout=None):
+def specsearch(ra,dec,radius,url,format=None,timeout=None):
     """
     Search for objects inside the circle (ra,dec,radius) at given 'url'
 
@@ -68,6 +124,8 @@ def specsearch(ra,dec,radius,url,timeout=None):
                  search radius [degrees]
      - url     : string
                  url of the service to query/retrieve
+     - format  : string
+                 format of spectra file(s) to be returned
      - timeout : int
                  time (seconds) to wait for a response
 
@@ -78,8 +136,8 @@ def specsearch(ra,dec,radius,url,timeout=None):
     """
     db_url = url
 
-    logging.debug("Position (%s,%s) and radius (%s), in degrees", ra, dec, radius)
-    logging.debug("URL (%s) and timeout (%s)", url, timeout)
+    logging.debug("Position (%s,%s) and radius (%s), in degrees" %(ra, dec, radius))
+    logging.debug("URL (%s) and timeout (%s)" % (url, timeout))
 
     from pyvo.dal import ssa
     from pyvo.dal import query
@@ -95,10 +153,11 @@ def specsearch(ra,dec,radius,url,timeout=None):
     q = ssa.SSAQuery(url)
     q.pos = (ra,dec)
     q.size = radius
+    if format:
+        q.format = format
 
     try:
         res = q.execute()
-        #res = ssa.search( db_url, (ra,dec), radius)
     except query.DALServiceError as e:
         logging.exception("DALServiceError raised: %s"%(e))
     except query.DALQueryError as e:
@@ -106,16 +165,17 @@ def specsearch(ra,dec,radius,url,timeout=None):
     except Exception as e:
         logging.exception("Exception raised: %s"%(e))
 
-    if res is None:
-        return None
-
     return res
 
+def concatenate_tables(tables):
+    from astropy.table import vstack
+    tab = vstack(tables, join_type='outer')
+    return tab
+    
 # --
 def main(ra,dec,radius,url,columns=[]):
     """
     Query SSA service and return a 'columns'-filtered table
-
 
     Input:
      - ra      : float
@@ -144,13 +204,26 @@ def main(ra,dec,radius,url,columns=[]):
         logging.error("Search failed to complete. Service not working properly. Exiting")
         return None
 
-    nobjs = ssaTab.nrecs
-    assert(nobjs == ssaTab.votable.nrows)
+    #TODO: How to deal with multiple results?!!!
+    tables = []
+    for rec in ssaTab:
+        filecache = Aux.download_spec(rec)
+        if 'fits' in rec.format:
+            format = 'fits'
+        elif 'votable' in rec.format:
+            format = 'votable'
+        else:
+            format = None
+        tab = Aux.open_spec(filecache,format)
+        if tab is None:
+            continue
+        tables.append(tab)
 
-    tab = ssaTab.votable.to_table()
-    if tab is None:
-        logging.error("Retrieved table is Null. Exiting")
+    if len(tables) == 0:
+        logging.error("No tables were found. Exiting")
         return None
+
+    tab = concatenate_tables(tables)
 
     # Garantee we don't have empty column names and names that match tble ones..
     if columns:
